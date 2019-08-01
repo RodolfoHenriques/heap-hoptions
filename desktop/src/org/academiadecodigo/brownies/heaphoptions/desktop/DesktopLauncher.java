85408d88fbd5b0c154e06994eab7c40f0768a5cd
Rodolfo Henriques <rodolfo.henriques.uc@gmail.com>
2019-08-01 16:13:00 +0100

    libGDX Base Project

---
+++ desktop/src/org/academiadecodigo/brownies/heaphoptions/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.brownies.heaphoptions.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.brownies.heaphoptions.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
